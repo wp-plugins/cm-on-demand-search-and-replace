2817140e046d14b81d49ccb01918b0db8fb95071
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 08:29:07 +0000

    update new version
    git-svn-id: https://plugins.svn.wordpress.org/cm-on-demand-search-and-replace/trunk@1141883 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/backend/admin_pro.php
@@ -0,0 +1,31 @@
+<div class="clear"></div>
+<hr>
+
+<h1>
+    <strong><?php echo CMODSAR_NAME; ?> Pro Version</strong>
+</h1>
+
+<p>
+    If you like the <?php echo CMODSAR_NAME; ?> but you're missing some features, probably you can find it in the Premium version.
+    <br/>
+    We have prepared lists of features which you can find in one of the Premium version of <a title="<?php echo CMODSAR_NAME; ?>" href="https://plugins.cminds.com/cm-on-demand-search-and-replace-plugin-for-wordpress/"><?php echo CMODSAR_NAME; ?></a>.
+</p>
+
+<div class="clear"></div>
+
+<br/><br/>
+
+<h2>Features (Pro Version Only)</h2>
+
+<ul>
+    <li><strong>Support custom post types</strong> - free version works only for posts and pages</li>
+    <li><strong>Supports timeframes</strong> - for temporary changes, future changes, or changes with expiry date</li>
+    <li><strong>Supports titles, comments and excerpts</strong> - you can setup a rule which does Search &amp; Replace eg. only in the comments</li>
+    <li><strong>Supports Advanced Custom Fields content</strong> - change within the ACF fields</li>
+    <li><strong>Supports Regex</strong> - a powerful tool for the ones knowing how to use it</li>
+    <li><strong>Disable per post/page</strong> - metabox allowing to choose the pages/posts excluded from the Search &amp; Replace </li>
+    <li><strong>Support specific rules per specific content</strong></li>
+</ul>
+
+<h1><strong>Purchase </strong></h1>
+<p>You can purchase the Pro version <a class="button button-primary" href="https://plugins.cminds.com/cm-on-demand-search-and-replace-plugin-for-wordpress/">Here</a></p>
